3e09cc4b4c6f114b376def1034f5d8679bd1a873
Eric Madar <emsgeek@gmail.com>
2012-01-02 15:49:34 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Health Timeline Project
+
+### What it is
+  Originally Planned as a health-related project for a design class,
+this site is also our first real foray into rails development from
+start to finish
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeline::Application.config.secret_token = 'efbe056f146d0bc37548aeb6dfaf2a0611fb65b5c1460274362a400f2427341b909c7f3e3006865570c08b0ff4300afad1cfdeabca9fff11ac4e11015c83c6f6'
